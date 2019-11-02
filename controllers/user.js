68ab9d0e427b41dd3eed9f0737589ed120ce35cf
amaitpaul2255 <55456860+amaitpaul2255@users.noreply.github.com>
2019-11-02 14:54:40 +0600

    Add files via upload

--- controllers/user.js
+++ controllers/user.js
@@ -18,5 +18,33 @@ router.get('/',function(request, response){
     
 });
 
+router.get('/abouthospital',function(request, response){   
+        response.render('user/abouthospital');   
+});
+
+router.get('/contactus',function(request, response){   
+    response.render('user/contactus');   
+});
+
+router.get('/department',function(request, response){   
+    response.render('user/department');   
+});
+
+router.get('/news',function(request, response){   
+    response.render('user/news');   
+});
+
+router.get('/narses',function(request, response){   
+    response.render('user/narses');   
+});
+
+router.get('/noticebord',function(request, response){   
+    response.render('user/noticebord');   
+});
+
+router.get('/gallery',function(request, response){   
+    response.render('user/gallery');   
+});
+
 
 module.exports = router;

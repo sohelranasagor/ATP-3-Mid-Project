008ace9bca2654ae7ee5d8285db9b83f003ba005
amaitpaul2255 <55456860+amaitpaul2255@users.noreply.github.com>
2019-11-03 20:57:02 +0600

    Add files via upload

--- models/user_model.js
+++ models/user_model.js
@@ -13,5 +13,17 @@ module.exports = {
 				callback([]);
 			}
 		});
-    }
+		
+		/*updateUser: function(data, callback){
+			var sql = "update users set fastname=?,lastname=?,email=?,dob=?,city=?,location=? where uid=?";
+			db.execute(sql, [data.password,data.id], function(status){
+				callback(status);
+			});*/
+	},
+	deleteuser: function(id, callback){
+		var sql = "delete from users where uid=?";
+		db.execute(sql, [id], function(status){
+			callback(status);
+		});
+	}
 }

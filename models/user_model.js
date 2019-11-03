3c909cdf6cbdcc69781e646506ac7d214de9e70e
amaitpaul2255 <55456860+amaitpaul2255@users.noreply.github.com>
2019-11-03 22:42:50 +0600

    Add files via upload

--- models/user_model.js
+++ models/user_model.js
@@ -14,16 +14,30 @@ module.exports = {
 			}
 		});
 		
-		/*updateUser: function(data, callback){
-			var sql = "update users set fastname=?,lastname=?,email=?,dob=?,city=?,location=? where uid=?";
-			db.execute(sql, [data.password,data.id], function(status){
-				callback(status);
-			});*/
+	
 	},
 	deleteuser: function(id, callback){
 		var sql = "delete from users where uid=?";
 		db.execute(sql, [id], function(status){
 			callback(status);
 		});
-	}
+	},
+	updateUser: function(user, callback){
+		var sql = "update users set firstname=?,lastname=?,dob=?,city=?,location=? where uid=?";
+		db.execute(sql, [user.fname,user.lname,user.dob,user.city,user.location,user.id], function(status){
+			callback(status);
+		});
+	},
+	getAllDoctor: function(callback){
+		var sql = "select * from doctor";
+		
+		db.getResults(sql, [], function(results){
+			
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback([]);
+			}
+		});	
+    }
 }
